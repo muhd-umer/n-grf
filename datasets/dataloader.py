b06888196addd00e3757174f8c0fa0257539499b
muhd-umer <thisismumer@gmail.com>
2025-04-23 18:07:37 +0500

    [minor] Update val batch size

--- datasets/dataloader.py
+++ datasets/dataloader.py
@@ -139,7 +139,7 @@ def get_dataloaders(
 
     val_loader = get_wireless_dataloader(
         data_path,
-        batch_size=batch_size,
+        batch_size=1,
         num_workers=num_workers,
         shuffle=False,
         train=False,
